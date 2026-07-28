f77792c7ffe973493aa602c551b1e6a4d5011e1f
Micha Schoell <8493022+schollm@users.noreply.github.com>
2026-07-28 08:43:09 +0000

    ruff format

--- src/check_dependencies/__main__.py
+++ src/check_dependencies/__main__.py
@@ -177,7 +177,7 @@ def main() -> int:
 
 class _MultiSepAction(argparse.Action):
     """Custom argparse action to split comma-separated values into a list.
-    
+
     This action allows the user to specify multiple values for an argument by
     separating them with commas. Each time the argument is encountered,
     the values are split and added to a list in the namespace.
